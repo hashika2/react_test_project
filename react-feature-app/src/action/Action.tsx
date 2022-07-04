208449ebb62300ba271bb9b674cda8260270f2d7
hashika <m.g.hashikamaduranga@gmail.com>
2022-07-04 11:41:14 +0530

    action create

---
+++ react-feature-app/src/action/Action.tsx
@@ -0,0 +1,17 @@
+export const Action1 = (action: any) => {
+    return(dispatch: any) => {
+        dispatch({
+            type: "Type1",
+            payload: action
+        })
+    }
+}
+
+export const Action2 = (action: any) => {
+    return(dispatch: any) => {
+        dispatch({
+            type: "Type2",
+            payload: action
+        })
+    }
+}
