ad8fe3e87178bbb4ace687148df58355ae51619d
hashika <m.g.hashikamaduranga@gmail.com>
2022-07-04 11:41:42 +0530

    create reducers

--- react-feature-app/src/state/TestReducer.tsx
+++ react-feature-app/src/state/TestReducer.tsx
@@ -5,22 +5,22 @@ interface State {
 
 interface Type1 {
     type: "Type1";
-    payload: string
+    payload: number
 }
 
 interface Type2 {
     type: "Type2";
-    payload: string
+    payload: number
 }
 
 type Action = Type1 | Type2;
 
-const reducer = (state: State, action: Action) => {
-    switch(action.type){
+const reducer = (state=0, action: Action) => {
+    switch(action.type) {
         case 'Type1':
             return state + action.payload;
         case 'Type2':
-            return state + action.payload;
+            return state - action.payload;
         default:
             return state;
             
---
+++ react-feature-app/src/state/index.tsx
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import reducer from "./TestReducer";
+
+ 
+ const reducres = combineReducers({
+    account: reducer
+ })
+
+ export default reducres;
