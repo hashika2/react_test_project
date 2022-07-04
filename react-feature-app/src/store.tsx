09bd3b249663e95f9e8ae59f99a31d9061e24e51
hashika <m.g.hashikamaduranga@gmail.com>
2022-07-04 11:42:48 +0530

    create a store

--- react-feature-app/src/store.tsx
+++ react-feature-app/src/store.tsx
@@ -1,12 +1,21 @@
-import {configureStore} from '@reduxjs/toolkit';
+// import {configureStore} from '@reduxjs/toolkit';
 
-const store = configureStore({
-    reducer:{
+import {applyMiddleware, createStore} from 'redux';
+import reducres from './state';
+import thunk from 'redux-thunk';
+// const store = configureStore({
+//     reducer:{
+ 
+//     }
+// })
 
-    }
-})
+// // Infer the `RootState` and `AppDispatch` types from the store itself
+// export type RootState = ReturnType<typeof store.getState>
+// // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+// export type AppDispatch = typeof store.dispatch
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
-export type RootState = ReturnType<typeof store.getState>
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
-export type AppDispatch = typeof store.dispatch
+export const store = createStore(
+    reducres,
+    {},
+    applyMiddleware(thunk)
+)
