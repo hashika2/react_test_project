4924a00bd3361b13b251681542d66503cd8bd7ad
hashika <m.g.hashikamaduranga@gmail.com>
2022-07-14 11:06:30 +0530

    remove unwanted ref

--- react-feature-app/src/App.tsx
+++ react-feature-app/src/App.tsx
@@ -1,7 +1,4 @@
-import { Route, Routes, useRoutes } from 'react-router-dom';
 import './App.css';
-import Test from './components/test1/Tset';
-import { Home } from './components/home/Home';
 import { Router } from './routes/Routes';
 
 function App() {
